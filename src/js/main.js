a07be978bbda2607273ee7ef7c2482c7b6de624e
Karlo Eleuterio <karloe@gmail.com>
2015-11-28 12:41:10 -0500

    revamping.. initialize bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,5 @@
 
 
 
-
-
-//nav stay top
-// credit to: http://stanhub.com/scroll-to-top-then-fixed-navigation-effect-with-jquery-and-css-free-download/
-
-$(document).ready(function(){
-	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 60;
-			 if ($(window).scrollTop() > navHeight) {
-				 $('.nav-main').addClass('stay-top');
-         $('.nav-filler').addClass('filling');
-			 }
-			 else {
-				 $('.nav-main').removeClass('stay-top');
-         $('.nav-filler').removeClass('filling');
-			 }
-		});
-
-    $('#nav-toggle').on('click', function (){
-      $('.nav-menu').slideToggle('50');
-    });
-
-		function blinker() {
-		  $('.cursor').fadeOut(300);
-		  $('.cursor').fadeIn(300);
-		}
-		setInterval(blinker, 600);
-
-
-	});
+	// $('Karlo.Eleuterio').ready('to-be-a-web-man')
+	// 		.value('frontEnd').company('  '); //TODO:insert company here
