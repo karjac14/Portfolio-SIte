b814f4b4f581fcb342350187e2f59c0bf7eb8511
Karlo Eleuterio <karloe@gmail.com>
2015-11-30 00:55:25 -0500

    portfolio hover and active working on desktop view

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,22 @@
 
 
-	// 
-	// $('Karlo.Eleuterio').ready('to-be-a-web-man')
-	// 		.value('frontEnd').company('  '); //TODO:insert company here
+$('#port-ziptrip').on('click', function(){
+  $(this).toggleClass('clicked');
+	$('#port-dyel,#port-kvsk').removeClass('clicked');
+  $('#port-ziptrip-long-desc').slideToggle('50');
+  $('#port-dyel-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
+});
+
+$('#port-dyel').on('click', function(){
+  $(this).toggleClass('clicked');
+	$('#port-ziptrip,#port-kvsk').removeClass('clicked');
+  $('#port-dyel-long-desc').slideToggle('50');
+  $('#port-ziptrip-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
+});
+
+$('#port-kvsk').on('click', function(){
+  $(this).toggleClass('clicked');
+	$('#port-dyel,#ziptrip').removeClass('clicked');
+  $('#port-kvsk-long-desc').slideToggle('50');
+  $('#port-ziptrip-long-desc,#port-dyel-long-desc').hide(); // to refactor?
+});
