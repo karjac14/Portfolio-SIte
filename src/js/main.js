5620bf13f30970bce65fad286522c10bcb51e281
Karlo Eleuterio <karloe@gmail.com>
2015-11-30 03:28:51 -0500

    add caret in porfolio descriptions

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 $('#port-ziptrip').on('click', function(){
   $(this).toggleClass('clicked');
 	$('#port-dyel,#port-kvsk').removeClass('clicked');
+	$('#about').addClass('down');
   $('#port-ziptrip-long-desc').slideToggle('50');
   $('#port-dyel-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
 });
@@ -10,6 +11,7 @@ $('#port-ziptrip').on('click', function(){
 $('#port-dyel').on('click', function(){
   $(this).toggleClass('clicked');
 	$('#port-ziptrip,#port-kvsk').removeClass('clicked');
+	$('#about').addClass('down');
   $('#port-dyel-long-desc').slideToggle('50');
   $('#port-ziptrip-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
 });
@@ -17,6 +19,7 @@ $('#port-dyel').on('click', function(){
 $('#port-kvsk').on('click', function(){
   $(this).toggleClass('clicked');
 	$('#port-dyel,#ziptrip').removeClass('clicked');
+	$('#about').addClass('down');
   $('#port-kvsk-long-desc').slideToggle('50');
   $('#port-ziptrip-long-desc,#port-dyel-long-desc').hide(); // to refactor?
 });
