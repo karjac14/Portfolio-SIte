c5cbf767200f7d93102dc8daf7bc561c043d340c
Karlo Eleuterio <karloe@gmail.com>
2015-11-30 12:51:03 -0500

    add about and contacts

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,24 @@
 
+//nav auto scroll
 
+$(function(){$("#nav-about, #learnmore").click(function(){
+  $("html,body").animate({scrollTop:$("#about").offset().top},"1000");return false})});
+
+$(function(){$("#nav-portfolio").click(function(){
+  $("html,body").animate({scrollTop:$("#portfolio").offset().top},"1000");return false})});
+
+$(function(){$("#nav-contact").click(function(){
+  $("html,body").animate({scrollTop:$("#contact").offset().top},"1000");return false})});
+
+
+
+// portfolio event listener
 $('#port-ziptrip').on('click', function(){
   $(this).toggleClass('clicked');
 	$('#port-dyel,#port-kvsk').removeClass('clicked');
 	$('#about').addClass('down');
   $('#port-ziptrip-long-desc').slideToggle('50');
-  $('#port-dyel-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
+  $('#port-dyel-long-desc,#port-kvsk-long-desc').hide();
 });
 
 $('#port-dyel').on('click', function(){
@@ -13,13 +26,13 @@ $('#port-dyel').on('click', function(){
 	$('#port-ziptrip,#port-kvsk').removeClass('clicked');
 	$('#about').addClass('down');
   $('#port-dyel-long-desc').slideToggle('50');
-  $('#port-ziptrip-long-desc,#port-kvsk-long-desc').hide(); // to refactor?
+  $('#port-ziptrip-long-desc,#port-kvsk-long-desc').hide();
 });
 
 $('#port-kvsk').on('click', function(){
   $(this).toggleClass('clicked');
-	$('#port-dyel,#ziptrip').removeClass('clicked');
+	$('#port-dyel,#port-ziptrip').removeClass('clicked');
 	$('#about').addClass('down');
   $('#port-kvsk-long-desc').slideToggle('50');
-  $('#port-ziptrip-long-desc,#port-dyel-long-desc').hide(); // to refactor?
+  $('#port-ziptrip-long-desc,#port-dyel-long-desc').hide();
 });
